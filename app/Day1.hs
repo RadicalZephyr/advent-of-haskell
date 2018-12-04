5a4669d00f5d5e702f56536589ea816de781e4f5
Geoff Shannon <geoffpshannon@gmail.com>
2018-12-04 03:52:14 -0800

    Complete day 1 part 1

--- app/Day1.hs
+++ app/Day1.hs
@@ -2,5 +2,15 @@ module Main where
 
 import Lib
 
-main :: IO ()
-main = someFunc
+main = do
+  input <- getContents
+  let frequencies = lines input in
+    putStrLn $ show $ sum $ map readInt $ frequencies
+
+part1 lines =
+  sum
+
+readInt :: String -> Int
+readInt whole@(head:tail)
+  | head == '+' = read tail
+  | otherwise = read whole
